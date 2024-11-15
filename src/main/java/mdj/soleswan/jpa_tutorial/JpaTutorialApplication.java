842b1a5cec44a8f83146e57128abfe1ba85690f0
soleswan <eowls1358@gmail.com>
2024-11-15 13:58:51 +0900

    1-1. insert 안됨 해결. Transaction get()

--- src/main/java/mdj/soleswan/jpa_tutorial/JpaTutorialApplication.java
+++ src/main/java/mdj/soleswan/jpa_tutorial/JpaTutorialApplication.java
@@ -2,6 +2,7 @@ package mdj.soleswan.jpa_tutorial;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 import mdj.soleswan.jpa_tutorial.entity.Member;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,12 +17,18 @@ public class JpaTutorialApplication {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); // persistence.xml 의 unit name
         EntityManager em = emf.createEntityManager(); // 일관적인 단위를 사용할 때 마다 생성해야함.
 
+        EntityTransaction ex = em.getTransaction();
+
+        ex.begin();
+
         Member member = new Member();
 
         member.setId(1L);
         member.setName("hello");
 
         em.persist(member);
+
+        ex.commit();
 
         //code
 
