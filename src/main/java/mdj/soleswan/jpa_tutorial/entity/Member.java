d185d88c86b7f26b95366d157b3e27b44f98f883
soleswan <eowls1358@gmail.com>
2024-11-15 17:35:46 +0900

    1. Member 필드와 컬럼 매핑 예시

--- src/main/java/mdj/soleswan/jpa_tutorial/entity/Member.java
+++ src/main/java/mdj/soleswan/jpa_tutorial/entity/Member.java
@@ -1,7 +1,14 @@
 package mdj.soleswan.jpa_tutorial.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,7 +21,22 @@ public class Member {
 
     @Id
     private long id;
-    private String name;
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creatDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
 
     public Member() {
 
---
+++ src/main/java/mdj/soleswan/jpa_tutorial/entity/RoleType.java
@@ -0,0 +1,5 @@
+package mdj.soleswan.jpa_tutorial.entity;
+
+public enum RoleType {
+    USER, ADMIN
+}
