7b12b5545ec4b5c6c239d6e9a7591d514415fbfa
soleswan <eowls1358@gmail.com>
2024-11-15 14:58:32 +0900

    1. persist() 함수는 1차 캐시에 데이터 저장.

--- src/main/java/mdj/soleswan/jpa_tutorial/JpaTutorialApplication.java
+++ src/main/java/mdj/soleswan/jpa_tutorial/JpaTutorialApplication.java
@@ -2,7 +2,9 @@ package mdj.soleswan.jpa_tutorial;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import mdj.soleswan.jpa_tutorial.entity.Member;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -14,9 +16,27 @@ public class JpaTutorialApplication {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
-        //code
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
 
-        em.close();
+        try {
+
+            // 비 영속성
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA");
+
+            // 영속성 컨텍스트 저장
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
 
     }
--- src/main/java/mdj/soleswan/jpa_tutorial/entity/Member.java
+++ src/main/java/mdj/soleswan/jpa_tutorial/entity/Member.java
@@ -1,5 +1,17 @@
 package mdj.soleswan.jpa_tutorial.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Member {
+
+    @Id
+    long id;
+    String name;
 
 }
