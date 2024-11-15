105c553f16eeaca085bee380cdfca9892ac4811b
soleswan <eowls1358@gmail.com>
2024-11-15 15:29:52 +0900

    Fix: 멤버 생성자 및 롬복 어노테이션 추가

--- src/main/java/mdj/soleswan/jpa_tutorial/entity/Member.java
+++ src/main/java/mdj/soleswan/jpa_tutorial/entity/Member.java
@@ -2,16 +2,21 @@ package mdj.soleswan.jpa_tutorial.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@AllArgsConstructor
 public class Member {
 
     @Id
-    long id;
-    String name;
+    private long id;
+    private String name;
 
+    public Member() {
+
+    }
 }
