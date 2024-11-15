5aaaa5156745cfcd607dfc5740a653fc687d1a79
soleswan <eowls1358@gmail.com>
2024-11-15 16:02:37 +0900

    8. 준영속 상태로 만들어줌 = 관리 제어 안함.

--- src/main/java/mdj/soleswan/jpa_tutorial/JpaTutorialApplication.java
+++ src/main/java/mdj/soleswan/jpa_tutorial/JpaTutorialApplication.java
@@ -21,17 +21,21 @@ public class JpaTutorialApplication {
 
         try {
 
-            Member member = new Member(300L, "member300");
-            em.persist(member);
+            Member member = em.find(Member.class, 103L);
+            member.setName("KKK");
 
-            em.flush();
+//            em.detach(member); // 준영속 상태로 만듬.
+            em.clear(); // entityManager 초기화
+
+            Member member2 = em.find(Member.class, 103L);
 
             System.out.println("=====================================");
+            System.out.println(member == member2);
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
         } finally {
-            em.close();
+            em.close(); // 종료
         }
         emf.close();
 
