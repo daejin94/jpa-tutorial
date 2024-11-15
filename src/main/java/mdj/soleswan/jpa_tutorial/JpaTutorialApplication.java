c44cdd7ebd736ed6b9ab6991b8b0bdfe18188689
soleswan <eowls1358@gmail.com>
2024-11-15 14:24:35 +0900

    5. JPQL 사용법

--- src/main/java/mdj/soleswan/jpa_tutorial/JpaTutorialApplication.java
+++ src/main/java/mdj/soleswan/jpa_tutorial/JpaTutorialApplication.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import java.util.List;
 import mdj.soleswan.jpa_tutorial.entity.Member;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,7 +15,12 @@ public class JpaTutorialApplication {
 
 //		SpringApplication.run(JpaTutorialApplication.class, args);
 
+        // db 당 하나. 실행되면 생성 됨.
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello"); // persistence.xml 의 unit name
+
+        // 요청 마다 생성 후 사용 하고 닫기.
+        // 절때 쓰레드 간 공유 X
+
         EntityManager em = emf.createEntityManager(); // 일관적인 단위를 사용할 때 마다 생성해야함.
 
         EntityTransaction ex = em.getTransaction();
@@ -22,10 +28,17 @@ public class JpaTutorialApplication {
         ex.begin();
 
         try {
-            Member findMember = em.find(Member.class, 2L);
 
-            findMember.setName("helloBB");
-            // set 하면 저장 따로 안해도 업데이트 됨 ㄷㄷ;
+            // 쿼리문을 쓴다고 해도, 객체 대상으로 쿼리 조회를 함.
+            List<Member> rs = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5)
+                    .setMaxResults(5)
+                    .getResultList();
+
+            for (Member member : rs) {
+                System.out.println("member.name = " + member.getName());
+            }
+
             ex.commit();
         } catch (Exception e) {
             ex.rollback();
